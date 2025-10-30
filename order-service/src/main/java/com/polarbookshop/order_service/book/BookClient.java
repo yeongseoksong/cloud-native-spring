503db1c015dfa06becba517723a3f2d8bf4ed0d1
yeongseoksong <deer159123@naver.com>
2025-10-31 00:27:57 +0900

    cc

--- order-service/src/main/java/com/polarbookshop/order_service/book/BookClient.java
+++ order-service/src/main/java/com/polarbookshop/order_service/book/BookClient.java
@@ -23,11 +23,11 @@ public class BookClient {
                 .uri(BOOK_ROOT_API + isbn)
                 .retrieve()
                 .bodyToMono(Book.class).
-                timeout(Duration.ofSeconds(3),Mono.empty())
-                .onErrorResume(WebClientResponseException.NotFound.class, e->Mono.empty())
                 // timeout -> retry  각 재시도마다 time out 적용 , retry-> timeout 전체 재시도를 포함해 time out 적용
+                timeout(Duration.ofSeconds(3),Mono.empty())
+                .onErrorResume(WebClientResponseException.NotFound.class, e->Mono.empty()) //404 에러 fallback
                 .retryWhen(
                         Retry.backoff(3,Duration.ofMillis(100))
-                ).onErrorResume(Exception.class, (e)->Mono.empty());
+                ).onErrorResume(Exception.class, (e)->Mono.empty()); // 3회 재시도 후에도 에러 fallback
     }
 }
