be14e4ed79bb7fdf537d5a93b21673048950499d
yeongseoksong <deer159123@naver.com>
2025-11-04 22:27:32 +0900

    init spring cloud function

---
+++ dispatcher-service/src/main/java/com/polarbookshop/dispatcher_service/DispatcherServiceApplication.java
@@ -0,0 +1,13 @@
+package com.polarbookshop.dispatcher_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispatcherServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DispatcherServiceApplication.class, args);
+	}
+
+}
